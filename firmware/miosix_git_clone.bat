c885e533fc40799bc31ec7dd0a35d43bf54e6f99
GenniImparato <imparato.genni@hotmail.it>
2021-03-20 01:20:17 +0100

    FIRST WORKING COMMIT
    Firmware:
    -miosix working
    -ADC driver working (single conversion)
    
    Hardware:
    -PCB quasi finita

---
+++ firmware/miosix_git_clone.bat
@@ -0,0 +1,6 @@
+git clone https://miosix.org/git-public/miosix-kernel.git
+cd miosix-kernel
+git fetch origin
+## To select the development version of the kernel
+#git checkout -b testing origin/testing
+exit
