b950910ffa75492cab346d12e8925ccdc71b18da
yazan-matar <yazan.matar@mail.polimi.it>
2021-04-15 18:04:46 +0200

    first trial on matrix led

--- firmware/source/main.cpp
+++ firmware/source/main.cpp
@@ -8,6 +8,9 @@
 using namespace miosix;
 
 typedef Gpio<GPIOA_BASE,1> analogIn;
+typedef Gpio<GPIOD_BASE,11> row1;
+typedef Gpio<GPIOD_BASE,9> row3;
+typedef Gpio<GPIOD_BASE,7> col7;
 
 AudioEffect			*effect;
 
@@ -15,6 +18,9 @@ AudioEffect			*effect;
 int main()
 {
 	analogIn::mode(Mode::INPUT_ANALOG);
+	row1::mode(Mode::OUTPUT);
+	row3::mode(Mode::OUTPUT);
+	col7::mode(Mode::OUTPUT);
 
 	effect = new Synthesizer();
 
@@ -37,9 +43,13 @@ int main()
 	//main loop
     while(true)
     {
-		ledOn();
-		Thread::sleep(1000);
-		ledOff();
-		Thread::sleep(1000);
+		// ledOn();
+		// Thread::sleep(1000);
+		// ledOff();
+		// Thread::sleep(1000);
+		row1::low();
+		col7::low();
+		row3::high();
+
     }
 }
